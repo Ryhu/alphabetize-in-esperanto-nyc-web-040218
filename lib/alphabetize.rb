18673cff187298a208e5bb9c9b006c94dec89ceb
Ryan Hu <ryanhuuu@gmail.com>
2018-03-29 03:01:02 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,6 @@ def alphabetize(arr)
   es_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   morph =       "1234567890abcdefghijklmnopqr"
   
-  retrun arr.sort_by {|word| word.tr(es_alphabet, morph)}
+  return arr.sort_by {|word| word.tr(es_alphabet, morph)}
   
 end
